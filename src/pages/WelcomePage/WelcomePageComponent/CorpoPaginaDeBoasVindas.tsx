034cdbcac95838e81cb764601b4a44d64aba5683
Luis Fylipe <x08521119160@tcu.gov.br>
2025-10-29 11:25:49 -0300

    feat(welcome-page): Corpo da pagina com foto e balões de fala conforme prototipo

---
+++ src/pages/WelcomePage/WelcomePageComponent/CorpoPaginaDeBoasVindas.tsx
@@ -0,0 +1,16 @@
+import balao from "../../../assets/imgs/vivo-119 1.png"
+import "../WelcomePageComponent/CorpoPaginaDeBoasVindas.css"
+
+export function CorpoPaginaDeBoasVindas() {
+    return (
+        <section className="corpo-pagina">
+        <div className="texto-principal">
+            <h1>"Venha fazer parte a plataforma da VIVO para sanar suas dúvidas"</h1>
+        </div>
+        <div className="balao">
+            <img src={balao}/>
+            <p>"Saiba como melhorar sua experiência de internet e resolver problemas de conexão com a Reclame Aqui VIVO"</p>
+        </div>
+        </section>
+    )
+}
