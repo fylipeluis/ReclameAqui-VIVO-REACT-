150056b9936fe200e2ac2203005eaac03e158222
Luis Fylipe <x08521119160@tcu.gov.br>
2025-10-30 20:25:04 -0300

    feat(welcome-page): Mudanças para melhor funcioanmento de imagem na pagina de boas vindas seguindo boas praticas

--- src/pages/WelcomePage/WelcomePageComponent/CorpoPaginaDeBoasVindas.tsx
+++ src/pages/WelcomePage/WelcomePageComponent/CorpoPaginaDeBoasVindas.tsx
@@ -4,13 +4,21 @@ import "../WelcomePageComponent/CorpoPaginaDeBoasVindas.css"
 export function CorpoPaginaDeBoasVindas() {
     return (
         <section className="corpo-pagina">
-        <div className="texto-principal">
-            <h1>"Venha fazer parte a plataforma da VIVO para sanar suas dúvidas"</h1>
-        </div>
-        <div className="balao">
-            <img src={balao}/>
-            <p>"Saiba como melhorar sua experiência de internet e resolver problemas de conexão com a Reclame Aqui VIVO"</p>
-        </div>
+            <div className="corpo-wrapper">
+                <div className="texto-principal">
+                    <h1>
+                        "Venha fazer parte a plataforma da VIVO para sanar suas dúvidas"
+                    </h1>
+                </div>
+
+                <div className="balao">
+                    <img src={balao} alt="boneco vivo" className="boneco-vivo" />
+                    <p>
+                        "Saiba como melhorar sua experiência de internet e resolver problemas
+                        de conexão com a Reclame Aqui VIVO"
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
