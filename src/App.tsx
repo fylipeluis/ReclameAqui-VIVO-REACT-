20ade3be2940328e5a5f1c733c4f2df516a7228a
Luis Fylipe <luisfylipe1802@gmail.com>
2025-11-25 11:04:22 -0300

    Ajustando import para  acabar com falhas em deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from './pages/WelcomePage/Welcome'
-import Login from './pages/LoginPage/login'
+import Login from './pages/LoginPage/Login'
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>  
     <Route path="/" element={<Welcome />}/>
-    <Route path="/login" element={<Login />}/>
+    <Route path="login" element={<Login />}/>
     </Routes>
   </BrowserRouter>
   )
